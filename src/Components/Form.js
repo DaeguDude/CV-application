45cd062a896f0cf9b6f9e24117f876794e242777
Sanghak <k3hppk@gmail.com>
2021-03-02 11:23:57 +0900

    Update Form item components
    It only had 1 item which was suited for creating 1 column form item.
    However, it also has 2 column items, so this will add component for
    2 column items.

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -9,4 +9,19 @@ function FormItem(props) {
   );
 }
 
-export default FormItem;
+function FormItemTwoColumns(props) {
+  return (
+    <div className="form__item flex-row">
+      <div className="flex-col full-width">
+        <label className="form__label">{props.firstColLabel}</label>
+        <input className="form__input" type="text" />
+      </div>
+      <div className="flex-col full-width flex__margin_l20">
+        <label className="form__label">{props.secondColLabel}</label>
+        <input className="form__input" type="text" />
+      </div>
+    </div>
+  );
+}
+
+export { FormItem, FormItemTwoColumns };
