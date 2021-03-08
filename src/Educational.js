cdd35f1163977b4de888572a6695fb37cd3e382a
Sanghak <k3hppk@gmail.com>
2021-03-08 23:16:11 +0900

    Move repeated code into Form component
    FormItemYear, FormItemMonth, Editbtn were repeated more than twice
    in the multiple places. This will move them into Form component so
    it can be handled from one place.

--- src/Educational.js
+++ src/Educational.js
@@ -1,8 +1,11 @@
 import React from 'react';
-import { FormItem, SubmitBtn } from './Form';
-import uniqid from 'uniqid';
-
-// Degree, city/town, school, date , description
+import {
+  FormItem,
+  SubmitBtn,
+  FormItemMonth,
+  FormItemYear,
+  EditBtn,
+} from './Form';
 
 class Educational extends React.Component {
   constructor(props) {
@@ -130,64 +133,6 @@ class FormDisplay extends React.Component {
         <EditBtn handleEdit={this.props.handleEdit} />
       </div>
     );
-  }
-}
-
-class FormItemMonth extends React.Component {
-  render() {
-    const { name, labelName, handleChange, value } = this.props;
-
-    return (
-      <div>
-        <label>{labelName}</label>
-        <select name={name} value={value} onChange={handleChange}>
-          <option>Don't Show This</option>
-          <option>Show Year Only</option>
-          <option>January</option>
-          <option>February</option>
-          <option>March</option>
-          <option>April</option>
-          <option>May</option>
-          <option>June</option>
-          <option>July</option>
-          <option>September</option>
-          <option>October</option>
-          <option>November</option>
-          <option>December</option>
-        </select>
-      </div>
-    );
-  }
-}
-
-class FormItemYear extends React.Component {
-  render() {
-    const { name, labelName, handleChange, value } = this.props;
-    const startYear = 1960;
-    const currentYear = 2021;
-    const emptyArray = Array(currentYear - startYear).fill(null);
-    const years = emptyArray.map((year, index) => {
-      return index + 1 + startYear;
-    });
-
-    // const listItems = numbers.map((number) => <li>{number}</li>);
-
-    return (
-      <div>
-        <label>{labelName}</label>
-        <select name={name} value={value} onChange={handleChange}>
-          {years.map((year) => (
-            <option key={uniqid()}>{year}</option>
-          ))}
-        </select>
-      </div>
-    );
-  }
-}
-
-class EditBtn extends React.Component {
-  render() {
-    return <button onClick={this.props.handleEdit}>Edit</button>;
   }
 }
 
--- src/Form.js
+++ src/Form.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import uniqid from 'uniqid';
 
 class FormItem extends React.Component {
   render() {
-    const { name, labelName, handleChange } = this.props;
+    const { name, labelName, handleChange, value } = this.props;
 
     return (
       <div>
         <label>{labelName}</label>
-        <input name={name} onChange={handleChange} type="text" />
+        <input name={name} value={value} onChange={handleChange} type="text" />
       </div>
     );
   }
@@ -15,12 +16,12 @@ class FormItem extends React.Component {
 
 class FormItemEmail extends React.Component {
   render() {
-    const { name, labelName, handleChange } = this.props;
+    const { name, labelName, handleChange, value } = this.props;
 
     return (
       <div>
         <label>{labelName}</label>
-        <input name={name} onChange={handleChange} type="email" />
+        <input name={name} value={value} onChange={handleChange} type="email" />
       </div>
     );
   }
@@ -28,12 +29,12 @@ class FormItemEmail extends React.Component {
 
 class FormItemTel extends React.Component {
   render() {
-    const { name, labelName, handleChange } = this.props;
+    const { name, labelName, handleChange, value } = this.props;
 
     return (
       <div>
         <label>{labelName}</label>
-        <input name={name} onChange={handleChange} type="tel" />
+        <input name={name} value={value} onChange={handleChange} type="tel" />
       </div>
     );
   }
@@ -41,19 +42,78 @@ class FormItemTel extends React.Component {
 
 class FormItemZip extends React.Component {
   render() {
-    const { name, labelName, handleChange } = this.props;
+    const { name, labelName, handleChange, value } = this.props;
 
     return (
       <div>
         <label>{labelName}</label>
         <input
           name={name}
+          value={value}
           onChange={handleChange}
           type="text"
           pattern="[0-9]*"
         />
       </div>
     );
+  }
+}
+
+class FormItemMonth extends React.Component {
+  render() {
+    const { name, labelName, handleChange, value } = this.props;
+
+    return (
+      <div>
+        <label>{labelName}</label>
+        <select name={name} value={value} onChange={handleChange}>
+          <option>Don't Show This</option>
+          <option>Show Year Only</option>
+          <option>January</option>
+          <option>February</option>
+          <option>March</option>
+          <option>April</option>
+          <option>May</option>
+          <option>June</option>
+          <option>July</option>
+          <option>September</option>
+          <option>October</option>
+          <option>November</option>
+          <option>December</option>
+        </select>
+      </div>
+    );
+  }
+}
+
+class FormItemYear extends React.Component {
+  render() {
+    const { name, labelName, handleChange, value } = this.props;
+    const startYear = 1960;
+    const currentYear = 2021;
+    const emptyArray = Array(currentYear - startYear).fill(null);
+    const years = emptyArray.map((year, index) => {
+      return index + 1 + startYear;
+    });
+
+    // const listItems = numbers.map((number) => <li>{number}</li>);
+
+    return (
+      <div>
+        <label>{labelName}</label>
+        <select name={name} value={value} onChange={handleChange}>
+          {years.map((year) => (
+            <option key={uniqid()}>{year}</option>
+          ))}
+        </select>
+      </div>
+    );
+  }
+}
+
+class EditBtn extends React.Component {
+  render() {
+    return <button onClick={this.props.handleEdit}>Edit</button>;
   }
 }
 
@@ -64,4 +124,13 @@ class SubmitBtn extends React.Component {
   }
 }
 
-export { FormItem, FormItemEmail, FormItemTel, FormItemZip, SubmitBtn };
+export {
+  FormItem,
+  FormItemEmail,
+  FormItemTel,
+  FormItemZip,
+  EditBtn,
+  SubmitBtn,
+  FormItemMonth,
+  FormItemYear,
+};
